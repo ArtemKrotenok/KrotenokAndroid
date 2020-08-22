61eabea4207e1302872f5ce93aab53ef2f1d2a6b
Artem Krotenok <artemkrot@mail.ru>
2020-08-22 10:48:04 +0300

    bug fixed

--- AndroidBeginner/app/src/main/java/com/gmail/krotenok/TextExchangeActivity.java
+++ AndroidBeginner/app/src/main/java/com/gmail/krotenok/TextExchangeActivity.java
@@ -18,28 +18,28 @@ public class TextExchangeActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_exchange);
-        initClickListener();
-    }
-
-    private void initClickListener() {
         button = (Button) findViewById(R.id.button1);
         textView1 = (TextView) findViewById(R.id.textView1);
         textView2 = (TextView) findViewById(R.id.textView2);
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CharSequence contentTextView1 = textView1.getText();
-                CharSequence contentTextView2 = textView2.getText();
-                Drawable backgroundTextView1 = textView1.getBackground();
-                Drawable backgroundTextView2 = textView2.getBackground();
-                textView1.setBackground(backgroundTextView2);
-                textView2.setBackground(backgroundTextView1);
-                textView1.setText(contentTextView2);
-                textView2.setText(contentTextView1);
+                changeContent();
             }
         };
         textView1.setOnClickListener(onClickListener);
         textView2.setOnClickListener(onClickListener);
         button.setOnClickListener(onClickListener);
+    }
+
+    private void changeContent() {
+        CharSequence contentTextView1 = textView1.getText();
+        CharSequence contentTextView2 = textView2.getText();
+        Drawable backgroundTextView1 = textView1.getBackground();
+        Drawable backgroundTextView2 = textView2.getBackground();
+        textView1.setBackground(backgroundTextView2);
+        textView2.setBackground(backgroundTextView1);
+        textView1.setText(contentTextView2);
+        textView2.setText(contentTextView1);
     }
 }
