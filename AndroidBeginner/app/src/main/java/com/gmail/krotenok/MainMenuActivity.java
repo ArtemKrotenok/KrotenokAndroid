60fb36f1884673a60718435ceea50d10d3f20cf2
Artem Krotenok <artemkrot@mail.ru>
2020-09-06 16:35:08 +0300

    dz_4 ready

--- AndroidBeginner/app/src/main/java/com/gmail/krotenok/MainMenuActivity.java
+++ AndroidBeginner/app/src/main/java/com/gmail/krotenok/MainMenuActivity.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 public class MainMenuActivity extends Activity {
     private Button buttonSelectTextExchangeActivity;
     private Button buttonSelectFlagsActivity;
+    private Button buttonSelectLoginActivity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +23,7 @@ public class MainMenuActivity extends Activity {
     private void initListenerButtonSelectActivity() {
         buttonSelectTextExchangeActivity = (Button) findViewById(R.id.button_menu_text_exchange_activity);
         buttonSelectFlagsActivity = (Button) findViewById(R.id.button_menu_flags_activity);
+        buttonSelectLoginActivity = (Button) findViewById(R.id.button_menu_login_activity);
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +37,10 @@ public class MainMenuActivity extends Activity {
                         intent = new Intent(MainMenuActivity.this, FlagsActivity.class);
                         break;
                     }
+                    case R.id.button_menu_login_activity: {
+                        intent = new Intent(MainMenuActivity.this, LoginLinearActivity.class);
+                        break;
+                    }
                     default:
                         intent = null;
                 }
@@ -45,5 +51,6 @@ public class MainMenuActivity extends Activity {
         };
         buttonSelectTextExchangeActivity.setOnClickListener(onClickListener);
         buttonSelectFlagsActivity.setOnClickListener(onClickListener);
+        buttonSelectLoginActivity.setOnClickListener(onClickListener);
     }
 }
