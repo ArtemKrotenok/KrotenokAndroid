d0cd4a3def3a5ce4f190b8ce265cce8dee16494f
Artem Krotenok <artemkrot@mail.ru>
2021-02-21 15:55:24 +0300

    deleted TaxiApp

--- TaxiApp/app/src/main/java/com/gmail/artemkrot/taxi/net/Callback.java
+++
@@ -1,5 +0,0 @@
-package com.gmail.artemkrot.taxi.net;
-
-public interface Callback {
-    void onResult(String data);
-}
--- TaxiApp/app/src/main/java/com/gmail/artemkrot/taxi/net/DataLoadListener.java
+++
@@ -1,5 +0,0 @@
-package com.gmail.artemkrot.taxi.net;
-
-public interface DataLoadListener {
-    void onFinishLoad();
-}
