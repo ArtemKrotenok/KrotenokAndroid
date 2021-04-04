82780bf76df18ee8d195a791489ef01fde8a4b0c
Artem Krotenok <artemkrot@mail.ru>
2021-04-04 22:53:34 +0300

    all bugs fixed, good test result

--- JokeAboutApp/app/src/main/java/com/gmail/artemkrot/jokeabout/JokeAlarmReceiver.java
+++ JokeAboutApp/app/src/main/java/com/gmail/artemkrot/jokeabout/JokeAlarmReceiver.java
@@ -4,13 +4,21 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
 public class JokeAlarmReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        OneTimeWorkRequest myWorkRequest = new OneTimeWorkRequest.Builder(JokeStartServiceWorker.class).build();
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+        OneTimeWorkRequest myWorkRequest = new OneTimeWorkRequest.Builder(JokeStartWorker.class)
+                .setConstraints(constraints)
+                .build();
         WorkManager.getInstance().enqueue(myWorkRequest);
     }
 }
--- JokeAboutApp/app/src/main/java/com/gmail/artemkrot/jokeabout/JokeStartWorker.java
+++ JokeAboutApp/app/src/main/java/com/gmail/artemkrot/jokeabout/JokeStartWorker.java
@@ -1,48 +1,48 @@
 package com.gmail.artemkrot.jokeabout;
 
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
-import android.os.IBinder;
 import android.util.Log;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
 import java.io.IOException;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class JokeService extends Service {
+public class JokeStartWorker extends Worker {
 
     private static final String URL_ADDRESS = "https://api.icndb.com/jokes/random";
     private static final String PARAM_NAME_FIRST_NAME = "firstName";
     private static final String PARAM_NAME_LAST_NAME = "lastName";
-    private static final String TAG = JokeService.class.getSimpleName();
+    private static final String TAG = JokeStartWorker.class.getSimpleName();
 
-    private Context context;
+    private final Context context;
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public JokeStartWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+        this.context = context;
     }
 
+    @NonNull
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        context = this;
+    public Result doWork() {
         PreferencesRepository preferencesRepository = new PreferencesRepository(context);
         String fistName = preferencesRepository.getFistNameValue();
         String lastName = preferencesRepository.getLastNameValue();
-        downloadAndShowJoke(fistName, lastName);
-        return super.onStartCommand(intent, flags, startId);
+        JokeUtil.setAlarmForShow(context);
+        if (downloadAndShowJoke(fistName, lastName)) {
+            return Result.success();
+        } else {
+            return Result.retry();
+        }
     }
 
-    private void downloadAndShowJoke(String fistName, String lastName) {
+    private boolean downloadAndShowJoke(String fistName, String lastName) {
         OkHttpClient client = new OkHttpClient();
         HttpUrl.Builder urlBuilder = HttpUrl.parse(URL_ADDRESS).newBuilder();
         urlBuilder.addQueryParameter(PARAM_NAME_FIRST_NAME, fistName);
@@ -51,19 +51,20 @@ public class JokeService extends Service {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.e(TAG, "fail to download data");
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
                 String responseData = response.body().string();
                 String messageNotification = JokeUtil.getTextJokeFromData(responseData);
                 String titleNotification = context.getString(R.string.title_notification);
                 NotificationUtil.showNotification(context, titleNotification, messageNotification);
+                return true;
+            } else {
+                Log.e(TAG, "fail to download data");
             }
-        });
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return false;
     }
 }
